d5190712d332c147cdb3d866d62716e3210dc6b7
ashwani-sinha <ashwaniraj084@gmail.com>
2026-02-16 13:32:24 +0530

    last commit

--- app_final_ai_detector.py
+++ app_final_ai_detector.py
@@ -281,5 +281,4 @@ def analyze():
 # ------------------ RUN ------------------
 
 if __name__ == "__main__":
-    print("Server Running: http://localhost:5001")
-    app.run(host="0.0.0.0", port=5001, debug=False)
+    app.run(host="0.0.0.0", port=5000)
